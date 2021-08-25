6b049b871f3df489a96a3103fc478118fc5fee2e
krifty <kaitlynmariejones@gmail.com>
2021-08-25 16:58:21 -0700

    form inpput makes book object

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibray = []
+let bookNum = 0
 
 
 function Book(title, author, page, read) {
@@ -16,31 +17,16 @@ function addBookToLibrary() {
 }
 
 function addBookSubmit() {
-    console.log("add book submit");
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let page = document.getElementById("page").value;
-    let statusRead = document.getElementById("have-read").checked ;
-    let statusNotRead = document.getElementById("have-not-read").checked ;
+    let readstatus = "blank"
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const page = document.getElementById("page").value;
+    const statusRead = document.getElementById("have-read").checked ;
+    const statusNotRead = document.getElementById("have-not-read").checked ;
+     
+    if (statusRead == true) {readstatus = "read"}
+    if (statusNotRead == true) {readstaus = "Not read"}
 
-    console.log(title);
-    console.log(author);
-    console.log(page);
-    console.log(statusRead);
-    console.log(statusNotRead);
-
-
+    let book =  new Book(title, author, page, readstatus)
+    myLibray.push(book)
 }
-
-//test array
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not yet read')
-let theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', '295', 'not yet read')
-let theHobbit3 = new Book('The Hobbit3', 'J.R.R. Tolkien', '295', 'not yet read')
-
-console.log(myLibray)
-myLibray.push(theHobbit)
-myLibray.push(theHobbit3)
-myLibray.push(theHobbit2)
-
-
-console.log(myLibray)
