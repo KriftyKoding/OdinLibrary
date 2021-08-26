dbacf59a5112931afeb296c5ddbd442a219ef2d4
krifty <kaitlynmariejones@gmail.com>
2021-08-26 11:24:14 -0700

    read status > changed how input works > removed range from display > percent if page numbers unknown > page read if known

--- script.js
+++ script.js
@@ -15,29 +15,42 @@ function Book(title, author, page, read) {
 //////////add book button//////////
 function addBookToLibrary() {
     console.log("add book");
-    removeBook();
-    console.log(myLibray)
+}
+
+
+
+function pageNumberChange(num) {
+    document.getElementById('readPercentValue').max = num
+    document.getElementById('readPercentValue').value = 0
+    rangeValue(0);
+}
+
+
+function rangeValue(val) {
+    if (document.getElementById('page').value == '') {
+        document.getElementById("readPercent").innerHTML = val + "%";
+    } else {
+        document.getElementById("readPercent").innerHTML = val + " pages";
+    }
+    
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////Form input create book object///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function addBookSubmit() {
-    let readstatus = "read status unknown"
+    // let readstatus = "read status unknown"
     const title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let page = document.getElementById("page").value;
-    const statusRead = document.getElementById("have-read").checked ;
-    const statusNotRead = document.getElementById("have-not-read").checked ;
-     
-    if (statusRead == true) {readstatus = "read"};
-    if (statusNotRead == true) {readstatus = "have not read"};
-    
+    let percentRead = document.getElementById("readPercentValue").value
+        
     if (title == "") {return};
     if (author == "") {author = "author unknown"};
     if (page == "") {page = "unknown"};
 
-    let book =  new Book(title, author, page, readstatus)
+    let book =  new Book(title, author, page, percentRead)
+    console.log(percentRead) + `ercent`;
     myLibray.push(book)
     
     displayLibrary();
@@ -46,9 +59,9 @@ function addBookSubmit() {
 
 
 //////////test book object//////////
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not yet read')
-let theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', '295', 'not yet read')
-let theHobbit3 = new Book('The Hobbit3', 'J.R.R. Tolkien', '295', 'not yet read')
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 25)
+let theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', '295', 100)
+let theHobbit3 = new Book('The Hobbit3', 'J.R.R. Tolkien', '295', 40)
 myLibray.push(theHobbit)
 myLibray.push(theHobbit2)
 myLibray.push(theHobbit3)
@@ -70,32 +83,27 @@ function displayLibrary(){
         const title = document.createElement('div');
         title.textContent = book.title;
         title.classList.add('title');
-        bookInfo.appendChild(title);
-
+        
         const author = document.createElement('div');
         author.textContent = book.author;
         author.classList.add('author');
-        bookInfo.appendChild(author);
         
         const page = document.createElement('div');
         page.textContent = book.page;
         page.classList.add('page');
-        bookInfo.appendChild(page);
-
-        const readStatus = document.createElement('INPUT');
-        readStatus.setAttribute("type", "range")
-        readStatus.setAttribute("min", "0")
-        readStatus.setAttribute("max", "100")
-        // add inut type
+        
+        
+        
+        ///////// READ STATUS Range and Label /////////////////
+        const readStatus = document.createElement('div');
         readStatus.classList.add('readStatus');
-        bookInfo.appendChild(readStatus);
-        
-        const readStatusLabel = document.createElement('Label');
-        readStatusLabel.textContent = "Amount Read";
-        readStatusLabel.classList.add('readStatus');
-        bookInfo.appendChild(readStatusLabel);
-
-        
+        if (book.page == "unknown") {
+            readStatus.innerHTML = book.read + '%'
+        }
+        else {
+            readStatus.innerHTML = book.read + ' pages read.'
+        }
+                
         const deleteBook = document.createElement('BUTTON');
         deleteBook.setAttribute("type", "button")
         deleteBook.setAttribute("onclick", `removeBook(${atrributeID})`)
@@ -103,9 +111,13 @@ function displayLibrary(){
         // console.log(atrributeID);
         deleteBook.textContent = 'Delete Book';
         deleteBook.classList.add('deleteBook');
+        
+        
+        bookInfo.appendChild(title);
+        bookInfo.appendChild(author);
+        bookInfo.appendChild(page);
+        bookInfo.appendChild(readStatus);
         bookInfo.appendChild(deleteBook);
-
-            
         bookList.appendChild(bookInfo);
     });
 }
@@ -120,4 +132,9 @@ function removeLibrary(){
 function removeBook(num){
     myLibray.splice(num, 1);
     displayLibrary();
+}
+
+//////// Change Number of Pages ////////
+function pageChange(num) {
+    
 }
